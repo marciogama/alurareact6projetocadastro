c8857cf6b920f536cdf4ad10451b34fd76d2650f
marciogama <marciogama2000@gmail.com>
2020-11-23 14:14:56 -0300

    adicionando validação do tamanho do campo CPF

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component{
     return (
       <Container component="article" maxWidth="sm">
         <Typography variant="h3" component="h1" align="center">Formulário de cadastro</Typography> 
-        <FormularioCadastro aoEnviar={aoEnviarForm}/>
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF}/>
       </Container>
     );
   }
@@ -17,6 +17,14 @@ class App extends Component{
 
 function aoEnviarForm(dados) {
   console.log(dados)
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11) {
+    return {valido:false, texto:"O CPF deve ter 11 dígitos !"};
+  } else {
+    return {valido:true, texto:""};
+  }
 }
 
 export default App;
